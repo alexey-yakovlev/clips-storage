35e7c0424c8c2883810ddde64744cc0c3e4d9d65
Alexei Yakovlev <morrigard@gmail.com>
2020-11-26 08:48:37 +0300

    change config

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 const PORT = process.env.REACT_APP_PORT || 8080;
-const developmentUrl = `http://localhost:${PORT}`;
+const developmentUrl = ''; // better use: "proxy": "http://localhost:8080", or `http://localhost:${PORT}` with { withCredentials: true } in axios
 const productionUrl = window.location.origin;
 export const HOST = process.env.NODE_ENV === 'production' ? productionUrl : developmentUrl;
